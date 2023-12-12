d27efe852447576b204895f53af5381e71c742c8
Asiful Kabir <akabir@waketech.edu>
2023-12-11 19:35:56 -0500

    Adding Book Class

---
+++ Books.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, title, author, ISBN, num_pages):
+        self.title = title
+        self.author = author
+        self.ISBN = ISBN
+        self.num_pages = num_pages
+        self.is_available = True
+
+    def display_info(self):
+        print(f"Title: {self.title}\nAuthor: {self.author}\nISBN: {self.ISBN}\nPages: {self.num_pages}\nAvailable: {self.is_available}\n")
+
+book1 = Book("Python Workshop", "XYZ", "123456789", 500)
+book1.display_info()
